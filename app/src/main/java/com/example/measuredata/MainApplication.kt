0c95c42ba92b313c836670ea6688f520c34275db
maselof <mr.gindeev@gmail.com>
2022-05-13 04:13:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/measuredata/MainApplication.kt
@@ -0,0 +1,12 @@
+package com.example.measuredata
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Application class, needed to enable dependency injection with Hilt.
+ */
+@HiltAndroidApp
+class MainApplication : Application()
+
+const val TAG = "Measuring Data Sample"
